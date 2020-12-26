4b1a0c783f4db3a3b8e0689014fa23da02797a23
Rasul <rasulirmatov@gmail.com>
2020-12-26 08:11:48 +0500

    adding all res files

---
+++ app/src/main/java/com/example/metproject/ForgotPasswordFragment.kt
@@ -0,0 +1,46 @@
+package com.example.metproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.metproject.databinding.FragmentForgotPasswordBinding
+import com.example.metproject.databinding.FragmentSignInBinding
+
+class ForgotPasswordFragment : Fragment() {
+    private lateinit var binding: FragmentForgotPasswordBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val dataBinding = DataBindingUtil.inflate<FragmentForgotPasswordBinding>(
+            inflater,
+            R.layout.fragment_forgot_password,
+            container,
+            false
+        )
+        dataBinding.lifecycleOwner = this
+        binding = dataBinding
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.sendRequestRecoveryButton.setOnClickListener { view ->
+            view?.let {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_forgotPasswordFragment_to_passwordRecoveryFragment)
+            }
+        }
+        binding.backBtn.setOnClickListener {
+            view?.let {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_forgotPasswordFragment_to_signInFragment)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/metproject/models/HomeSliderModel.kt
@@ -0,0 +1,7 @@
+package com.example.metproject.models
+
+data class HomeSliderModel (
+    val title: String,
+    val description: String,
+    val icon: Int
+)
